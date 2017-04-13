31d915772505a7587e2d7712aa2a9b4725979548
Nodul <kamil.aniszkiewicz@gmail.com>
2017-04-13 22:10:15 +0200

    r32 - added lesson 32 - Basic location quest + location triggers. Also updated the release build

---
+++ Assets/Scripts/QuestTrigger.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestTrigger : MonoBehaviour {
+
+    private QuestManager qM;
+
+    public int QuestNumber;
+
+    public bool startQuest;
+    public bool endQuest;
+
+	// Use this for initialization
+	void Start () {
+        qM = FindObjectOfType<QuestManager>();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            if (!qM.questsCompleted[QuestNumber])
+            {
+                if (startQuest && !qM.quests[QuestNumber].gameObject.activeSelf)
+                {
+                    qM.quests[QuestNumber].gameObject.SetActive(true);
+                    qM.quests[QuestNumber].StartQuest();
+                }
+
+                if (endQuest && qM.quests[QuestNumber].gameObject.activeSelf)
+                {
+                    qM.quests[QuestNumber].EndQuest();
+                    // qM.quests[QuestNumber].gameObject.SetActive(true);
+                   
+                }
+            }
+        }
+    }
+}
