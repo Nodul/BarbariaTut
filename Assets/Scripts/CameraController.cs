f54c5dd184b06b60c63e25ec5531aa7641d95e45
Nodul <kamil.aniszkiewicz@gmail.com>
2017-05-02 00:18:44 +0200

    r36 - Camera bounds

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class CameraController : MonoBehaviour {
 
@@ -8,6 +9,14 @@ public class CameraController : MonoBehaviour {
     public float moveSpeed;
 
     private static bool cameraExists;
+
+    public BoxCollider2D BoundBox; // each map has it's own!
+    Vector3 minBounds;
+    Vector3 maxBounds;
+
+    Camera Camera;
+    float halfHeight;
+    float halfWidth;
 
 	// Use this for initialization
 	void Start () {
@@ -23,10 +32,15 @@ public class CameraController : MonoBehaviour {
         {
             Destroy(gameObject);
         }
+        SetBounds();
+        this.Camera = this.GetComponent<Camera>();
+        halfHeight = Camera.orthographicSize;
+        halfWidth = halfHeight * Screen.width / Screen.height;
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+
+    // Update is called once per frame
+    void Update () {
 
         targetPos = new Vector3(
             followTarget.transform.position.x,
@@ -38,5 +52,20 @@ public class CameraController : MonoBehaviour {
             targetPos,
             moveSpeed * Time.deltaTime);
 
-	}
+        float clampedX = Mathf.Clamp(transform.position.x,minBounds.x + halfWidth, maxBounds.x - halfWidth);
+        float clampedY = Mathf.Clamp(transform.position.y, minBounds.y + halfHeight, maxBounds.y - halfHeight);
+
+        transform.position = new Vector3(clampedX,clampedY,this.transform.position.z);
+    }
+
+    public void SetBounds()
+    {
+        BoundBox = GameObject.FindGameObjectWithTag("BoundsBox").GetComponent<BoxCollider2D>();
+        minBounds = BoundBox.bounds.min;
+        maxBounds = BoundBox.bounds.max;
+    }
+    private void OnLevelWasLoaded(int level)
+    {
+        SetBounds();
+    }
 }
