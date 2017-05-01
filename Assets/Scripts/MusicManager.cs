5d59304d15a3de2382e9e1296bbb11d0e3fbfea0
Nodul <kamil.aniszkiewicz@gmail.com>
2017-05-02 01:22:24 +0200

    r38 - Adde music

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+    public AudioSource[] MusicTracks;
+
+    public int CurrentTrack;
+
+    public bool MusicCanPlay;
+
+    private static bool MusicManExists;
+    // Use this for initialization
+    void Start()
+    {
+        if (!MusicManExists)
+        {
+            MusicManExists = true;
+            DontDestroyOnLoad(transform.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        if (MusicCanPlay)
+        {
+            if (!MusicTracks[CurrentTrack].isPlaying)
+            {
+                MusicTracks[CurrentTrack].Play();
+            }
+        }
+        else
+        {
+            MusicTracks[CurrentTrack].Stop();
+        }
+    }
+
+    public void SwitchTrack(int newTrack)
+    {
+        MusicTracks[CurrentTrack].Stop();
+        CurrentTrack = newTrack;
+        MusicTracks[CurrentTrack].Play();
+
+    }
+}
---
+++ Assets/Scripts/MusicSwitcher.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicSwitcher : MonoBehaviour {
+
+    private MusicManager mcMan;
+
+    public int NewTrack;
+
+    public bool SwitchOnStart;
+
+	// Use this for initialization
+	void Start () {
+        mcMan = FindObjectOfType<MusicManager>();
+
+        if (SwitchOnStart)
+        {
+            mcMan.SwitchTrack(NewTrack);
+            gameObject.SetActive(false);
+        }
+	}	
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.name == "Player")
+        {
+            mcMan.SwitchTrack(NewTrack);
+            gameObject.SetActive(false); // So you won't reset the track multiple times 
+        }
+    }
+}
